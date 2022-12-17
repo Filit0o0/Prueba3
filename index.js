c976ee01943f21779ef458b4b2e05d0bc83b95fa
Filit0o0 <120840092+Filit0o0@users.noreply.github.com>
2022-12-17 16:15:10 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Sabia que ibas a decir que si mamacita, uwu 😈🥵')
+})
+
+noBtn.addEventListener('click',function () {
+    alert('Aun asi te cogeremos cosita deliciosa🥵')
+})
